b00d318170528f3775947eef4d7bbf300d404b41
Madhu <madhuthakur2321998@gmail.com>
2026-05-21 01:18:49 +0530

    Added email verification feature

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,24 +1,94 @@
 import "../styles/welcome.css";
-
 import { auth } from "../firebase";
 
-function Welcome({
-  openProfile,
-}) {
-  const isProfileComplete =
-    auth.currentUser?.displayName &&
-    auth.currentUser?.photoURL;
+function Welcome({ openProfile }) {
+  const user = auth.currentUser;
+ 
+  const handleVerifyEmail = async () => {
+    try {
+ 
+      const token =
+        await user.getIdToken();
+ 
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${
+          import.meta.env.VITE_FIREBASE_API_KEY
+        }`,
+        {
+          method: "POST",
+
+          headers: {
+            "Content-Type":
+              "application/json",
+          },
+ 
+          body: JSON.stringify({
+            requestType:
+              "VERIFY_EMAIL",
+
+            idToken: token,
+          }),
+        }
+      );
+
+      const data =
+        await response.json();
+ 
+      if (!response.ok) {
+        throw new Error(
+          data.error.message
+        );
+      }
+ 
+      alert(
+        "Verification email sent successfully"
+      );
+
+    } catch (error) {
+
+      alert(error.message);
+    }
+  };
 
   return (
     <div className="welcome-page">
       <div className="welcome-box">
+
         <h2>
           Welcome to Expense
           Tracker!!!
         </h2>
+ 
+        {user?.displayName ? (
+          <div className="success-alert">
 
-        {!isProfileComplete ? (
+            <span>
+              Profile completed
+              successfully 😀
+            </span>
+
+           
+            {user.emailVerified ? (
+
+              <button disabled>
+                Email Verified 
+              </button>
+
+            ) : (
+              <button
+                onClick={
+                  handleVerifyEmail
+                }
+              >
+                Verify Email
+              </button>
+            )}
+
+          </div>
+        ) : (
+
           <div className="profile-alert">
+
             <span>
               Your profile is
               incomplete.
@@ -30,15 +100,10 @@ function Welcome({
             >
               Complete now
             </button>
-          </div>
-        ) : (
-          <div className="profile-complete">
-            <span>
-              Profile completed
-              successfully 😀
-            </span>
+
           </div>
         )}
+
       </div>
     </div>
   );
