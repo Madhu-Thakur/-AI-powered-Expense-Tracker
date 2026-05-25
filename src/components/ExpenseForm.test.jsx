81545d27155671ced5dff1db1ad6495ce5a61cde
Madhu <madhuthakur2321998@gmail.com>
2026-05-25 13:49:25 +0530

    10 test case

--- src/components/ExpenseForm.test.jsx
+++ src/components/ExpenseForm.test.jsx
@@ -1,17 +1,152 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ExpenseForm from './ExpenseForm';
 
 describe('ExpenseForm Component', () => {
 
   test('renders add expense heading', () => {
 
-    render(<ExpenseForm />);
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
 
-    const textElement = screen.getByRole('heading', {
-  name: 'Add Expense',
-});
+    const headingElement =
+  screen.getByRole('heading', {
+    name: 'Add Expense',
+  });
 
-    expect(textElement).toBeTruthy();
+    expect(headingElement).toBeTruthy();
+
+  });
+
+  test('renders amount input', () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+    const amountInput =
+      screen.getByPlaceholderText(
+        'Enter Amount'
+      );
+
+    expect(amountInput).toBeTruthy();
+
+  });
+
+  test('renders description input', () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+    const descriptionInput =
+      screen.getByPlaceholderText(
+        'Enter Description'
+      );
+
+    expect(descriptionInput).toBeTruthy();
+
+  });
+
+  test('typing works in amount input', async () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+    const input =
+      screen.getByPlaceholderText(
+        'Enter Amount'
+      );
+
+    await userEvent.type(input, '500');
+
+    expect(input).toBeTruthy();
+
+  });
+
+  test('typing works in description input', async () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+    const input =
+      screen.getByPlaceholderText(
+        'Enter Description'
+      );
+
+    await userEvent.type(input, 'Pizza');
+
+    expect(input).toBeTruthy();
+
+  });
+
+  test('add expense button exists', () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+   const button =
+  screen.getByRole('button', {
+    name: 'Add Expense',
+  });
+
+    expect(button).toBeTruthy();
+
+  });
+
+  test('button is clickable', async () => {
+
+    render(
+      <ExpenseForm
+        amount=""
+        setAmount={() => {}}
+        description=""
+        setDescription={() => {}}
+      />
+    );
+
+    const button =
+  screen.getByRole('button', {
+    name: 'Add Expense',
+  });
+
+    await userEvent.click(button);
+
+    expect(button).toBeTruthy();
 
   });
 
